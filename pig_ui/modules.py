87270033dd9943912af356ca4784cd7b4c284ae6
Justus <justus.d2025@gmail.com>
2025-11-19 02:20:53 +0100

    changed imports & added defaults for packages

--- pig_ui/modules.py
+++ pig_ui/modules.py
@@ -1,32 +1,16 @@
+import json, os
+import random
+from typing import Callable
+from time import time
+
 try:
-    from pygame import Color, Surface, Vector2, Rect
-    from pygame.surfarray import make_surface
-
     import pygame as pg
-    import json, os
-    import random
-    from time import time
-    pg.font.init()
-    from typing import Callable
+    from pygame import Rect, Vector2, Surface, Color
+    from pygame.surfarray import make_surface
+    PG = pg
 except:
-    ...
-
-# A workaround for the error: subprocess-exited-with-error
-# Fixed by simply not packaging pygame into the package
-
-class pygame2:
-    def set_pygame(self, module):
-        pass
-    class Color: ...
-    class Surface: ...
-    class Vector2: ...
-    class Rect: ...
-    class font:
-        def init(): ...
-    class mouse:
-        def get_pos(): ...
-    class draw:
-        def circle(): ...
-        def rect(): ...
-
-PC = pygame2()
+    def Color(*C): ...
+    def Rect(*RV): ...
+    def Vector2(*V): ...
+    def Surface(*P): ...
+    PG = None
