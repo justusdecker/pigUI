168a92605f1da5f7c890b674eccfea0b5ef82acf
Justus <justus.d2025@gmail.com>
2025-11-19 15:58:47 +0100

    added documentation progress

--- tools/auto_docs.py
+++ tools/auto_docs.py
@@ -10,6 +10,12 @@ def get_python_paths() -> list[str]:
                 _ret.append(p)
     return _ret
 
+def get_status(f: int) -> str:
+    return ['🔴','🟢','🟡'][f]
+
+def get_table_element(p:str, m:str, f: int, t: str) -> str:
+    return f'|[{m}](../{p.replace("./","")})|{t}|{get_status(f)}|\n'
+
 def pprint(p:str, m: str, f: int) -> None:
     c = ['\033[33mNODOC', '\033[32mFOUND', '\033[35mSHORT'][f]
     print(f'[{c}\033[0m]\033[35m {p}\033[0m >> \033[34m{m}\033[0m')
@@ -21,6 +27,7 @@ def parse_files(skip_contains: list[str], paths: list[str] = get_python_paths())
     nodoc = 0
     numdocs = 0
     MD = "# Automatic-Documentator by Justus Decker\n\n"
+    DOCS_MD = '# Documentation Status\n\n|file(object)|type|status|\n|---|---|---|\n'
     for path in paths:
         if any([sc in path for sc in skip_contains]): continue
             
@@ -28,7 +35,7 @@ def parse_files(skip_contains: list[str], paths: list[str] = get_python_paths())
             data = file.read()
         tree = ast.parse(data)
         docs = ast.get_docstring(tree)
-
+        
         MD += f'# {path}\n\n'
         if docs is not None:
             MD += docs + '\n\n'
@@ -41,29 +48,38 @@ def parse_files(skip_contains: list[str], paths: list[str] = get_python_paths())
                 f_doc = ast.get_docstring(n)
                 if f_doc is None:
                     nodoc += 1
+                    DOCS_MD += get_table_element(path, n.name, 0, 'function')
                     pprint(path, n.name,0)
                     continue
                 if len(f_doc) <= 30:
+                    DOCS_MD += get_table_element(path, n.name, 2, 'function')
                     pprint(path, n.name,2)
                 else:
                     pprint(path, n.name,1)
+                    DOCS_MD += get_table_element(path, n.name, 1, 'function')
                 MD += f'## {n.name}\n{f_doc}\n\n'
+                
 
             if isinstance(n, ast.ClassDef):
                 numdocs += 1
                 c_doc = ast.get_docstring(n)
                 if c_doc is None:
                     nodoc += 1
+                    DOCS_MD += get_table_element(path, n.name, 0, 'class')
                     pprint(path, n.name,0)
                     continue
                 if len(c_doc) <= 30:
+                    DOCS_MD += get_table_element(path, n.name, 2, 'class')
                     pprint(path, n.name,2)
                 else:
+                    DOCS_MD += get_table_element(path, n.name, 1, 'class')
                     pprint(path, n.name,1)
                 MD += f'## {n.name}\n{c_doc}\n\n'
     print(f'\033[35m{(1 - (nodoc / numdocs)) * 100:.2f}% \033[0m(\033[34m{numdocs - nodoc}\033[0m of \033[34m{numdocs}\033[0m)')   
-    with open('./docs/auto-docs.md', 'w') as file:
-        file.write(MD)
+    with open('./docs/auto-docs.md', 'wb') as file:
+        file.write(MD.encode())
+    with open('./docs/docs-progress.md','wb') as file:
+        file.write(DOCS_MD.encode())
         
 if __name__ == '__main__':
     parse_files(
