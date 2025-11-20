b1d22d5d1cf09a0a071a4b0e0ff7bbb0fd83651b
Justus <justus.d2025@gmail.com>
2025-11-20 10:13:06 +0100

    add docs

--- pig_ui/ui/ui_dropdown.py
+++ pig_ui/ui/ui_dropdown.py
@@ -3,7 +3,12 @@ from pig_ui.ui.ux_element import UXWrapper, UXText, UXRect
 from pig_ui.ui.ui_element import UIElement
 from pig_ui.ui.ui_manager import UIM
 class UIDropDown(UIElement):
-    def __init__(self, app, pos, size, ux = None, draggable = False, **kwargs):
+    """
+    A DropDown Menu.
+    
+    - * **title**: changes the title of the main element
+    """
+    def __init__(self, app, pos, size, **kwargs):
         kwargs['cb_lclick'] = self.toggle_dd
         kwargs['anchor'] = "tl"
         ux=UXWrapper(
@@ -12,22 +17,53 @@ class UIDropDown(UIElement):
                  UXText(text_get_callback=kwargs.get('title', ''))] for col in ('#484848', '#969696', '#ffffff', '#000000')
             ]
         )
-        super().__init__(app, pos, size, ux, draggable, **kwargs)
+        super().__init__(app, pos, size, ux, False, **kwargs)
         self.sub = []
-        kwargs.get('ltext')
-        kwargs.get('lcom')
+
     @property
     def text(self) -> str:
+        """
+        !Unused
+        """
         return "test"
+    
     def toggle_dd(self,*_):
+        """
+        Toggles the visibility of all SubElements
+        """
         for uie in self.sub:
             uie.visible = not uie.visible
+            
     def sub_callbacks(self,obj):
+        """
+        Runs the corresponding callback for the dropdown subelement.
+        Determined by the object
+        """
         i = self.sub.index(obj)
         self.sub_cbs[i](obj)
         for uie in self.sub:
             uie.visible = not uie.visible
+            
     def set_subs(self, ltext: list[str], lcom: list[Callable] | None = None):
+        """
+        Set SubElements & Resets existing Subs if exist:
+        Format:
+        ltext:
+        ```json
+        [
+            "abc",
+            "def",
+            ...
+        ]
+        lcom:
+        None or
+        [
+            function,
+            lambda x: print(x),
+            ...
+        ]
+        ```
+        """
         for uie in self.sub:
             uie: UIElement
             uie.destroy()
@@ -35,10 +71,8 @@ class UIDropDown(UIElement):
         self.texts = []
         if lcom is None:
             lcom = [lambda *x: None for i in ltext]
-        print(ltext, lcom)
         self.sub_cbs = lcom
         for i, (t, c) in enumerate(zip(ltext, lcom)):
-            print(t,)
             self.texts.append(t)
             ux = [
                 [UXRect(-1,Color(col),size=self.size),
@@ -57,5 +91,4 @@ class UIDropDown(UIElement):
                 cb_dclick=lambda x: None,
                 visible=False
                 )
-            print(self.abs_offset + Vector2(0,(i+1) * self.size.y), uie.abs_offset)
             self.sub.append(uie)
