f83a690c6771b9bc85392a267803a16114ae32fc
Justus <justus.d2025@gmail.com>
2025-12-08 01:16:27 +0100

    [auto] update version

--- pig_ui/__init__.py
+++ pig_ui/__init__.py
@@ -1,21 +1,22 @@
 __version__ = "0.1.49"
-from pig_ui.ui.ux_element import (
+
+from .ui.ux_element import (
     UXWrapper, 
     UXText, 
     UXRect,
     UXImage,
     UXCircle
 )
-from pig_ui.ui.ui_element import UIElement
-from pig_ui.ui.ui_textinput import UITextInput
-from pig_ui.ui.ui_dropdown import UIDropDown
-from pig_ui.ui.ui_menu import UIMenuBar
-from pig_ui.ui.ui_colorpicker import UIColorPicker
-from pig_ui.ui.ui_spinbox import UISpinBox
-from pig_ui.ui.ui_sidebar import UISideBar
-from pig_ui.ui.constants import Anchors
+from .ui.ui_element import UIElement
+from .ui.ui_textinput import UITextInput
+from .ui.ui_dropdown import UIDropDown
+from .ui.ui_menu import UIMenuBar
+from .ui.ui_colorpicker import UIColorPicker
+from .ui.ui_spinbox import UISpinBox
+from .ui.ui_sidebar import UISideBar
+from .ui.constants import Anchors
 
-from pig_ui.ui.ui_manager import UIM
-from pig_ui.app import App
+from .ui.ui_manager import UIM
+from .app import App
 
-from pig_ui.ui.bezier import draw_beziere, compute_bezier_points
+from .ui.bezier import draw_beziere, compute_bezier_points
